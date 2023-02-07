3cfae2a8735e9f6372026d07b66b3031344ab604
syvt111 <tiensyvan@gmail.com>
2023-02-07 10:59:49 +0700

    nop bt

--- ss4_views_thymeleaf/product_manager/src/main/java/com/example/service/ProductService.java
+++ ss4_views_thymeleaf/product_manager/src/main/java/com/example/service/ProductService.java
@@ -1,73 +1,47 @@
 package com.example.service;
 
 import com.example.model.Product;
+import com.example.repository.IProductRepository;
+import com.example.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ProductService implements IProductService {
-    static List<Product> productList = new ArrayList<>();
 
-    static {
-        productList.add(new Product(1, "car", 12000));
-        productList.add(new Product(2, "mot0", 12000));
-        productList.add(new Product(3, "bycle", 12000));
-    }
+    IProductRepository productRepository = new ProductRepository();
 
+    @Override
     public List<Product> findAll() {
-        return productList;
+        return productRepository.findAll();
     }
 
     @Override
     public void save(Product product) {
-        productList.add(product);
+        productRepository.save(product);
+
     }
 
     @Override
     public void edit(Product product) {
-        for (Product product1 : productList
-        ) {
-            if (product1.getId() == product.getId()) {
-                product1.setName(product.getName());
-                product1.setPrice(product.getPrice());
-                break;
-            }
-        }
+        productRepository.edit(product);
+
     }
 
     @Override
     public Product findByID(int id) {
-        for (Product product : productList) {
-            if (id == product.getId()) {
-                return product;
-            }
-        }
-        return null;
+        return productRepository.findByID(id);
     }
 
     @Override
     public void remove(int id) {
-        Product product1 = null;
-        for (Product product : productList) {
-            if (id == product.getId()) {
-                product1 = product;
-                break;
-            }
-        }
-        productList.remove(product1);
+        productRepository.remove(id);
 
     }
 
     @Override
     public List<Product> search(String name) {
-        List<Product> products = new ArrayList<>();
-        for (Product product : productList) {
-            if (product.getName().contains(name)) {
-                products.add(product);
-            }
-        }
-        return products ;
+        return productRepository.search(name);
     }
 }
