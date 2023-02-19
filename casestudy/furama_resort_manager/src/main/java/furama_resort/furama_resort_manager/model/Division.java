2117aadff1931bf451890f62b11e6254c2d2332a
syvt111 <tiensyvan@gmail.com>
2023-02-19 11:43:10 +0700

    nop caseStudy

---
+++ casestudy/furama_resort_manager/src/main/java/furama_resort/furama_resort_manager/model/Division.java
@@ -0,0 +1,52 @@
+package furama_resort.furama_resort_manager.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+
+    @Entity
+    public class Division {
+        @Id
+        private int id;
+        @Column(columnDefinition = "varchar(45)")
+        private String name;
+
+        @OneToMany(mappedBy = "division")
+        private Set<Employee> employeeSet;
+
+        public Division() {
+        }
+
+        public Division(int id, String name, Set<Employee> employeeSet) {
+            this.id = id;
+            this.name = name;
+            this.employeeSet = employeeSet;
+        }
+
+        public Set<Employee> getEmployeeSet() {
+            return employeeSet;
+        }
+
+        public void setEmployeeSet(Set<Employee> employeeSet) {
+            this.employeeSet = employeeSet;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
